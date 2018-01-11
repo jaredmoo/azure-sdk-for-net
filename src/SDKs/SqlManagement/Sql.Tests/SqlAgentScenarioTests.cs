1883408cf8e2e1943adea991a56110905ac252d8
Jared Moore <jaredmoo@microsoft.com>
2018-01-10 17:50:57 -0800

    Added TestExecuteJob

--- src/SDKs/SqlManagement/Sql.Tests/SqlAgentScenarioTests.cs
+++ src/SDKs/SqlManagement/Sql.Tests/SqlAgentScenarioTests.cs
@@ -6,15 +6,7 @@ using Microsoft.Azure.Management.ResourceManager;
 using Microsoft.Azure.Management.ResourceManager.Models;
 using Microsoft.Azure.Management.Sql;
 using Microsoft.Azure.Management.Sql.Models;
-using Microsoft.Azure.Test.HttpRecorder;
-using Microsoft.Rest.Azure;
-using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Sql.Tests
@@ -30,7 +22,7 @@ namespace Sql.Tests
                 Server server = context.CreateServer(resourceGroup);
                 SqlManagementClient sqlClient = context.GetClient<SqlManagementClient>();
 
-                // Create database only required parameters
+                // Create database
                 string dbName = SqlManagementTestUtilities.GenerateName();
                 var db1 = sqlClient.Databases.CreateOrUpdate(resourceGroup.Name, server.Name, dbName, new Database()
                 {
@@ -66,6 +58,86 @@ namespace Sql.Tests
 
                 // Delete agent
                 sqlClient.JobAgents.Delete(resourceGroup.Name, server.Name, agentName);
+            }
+        }
+
+        [Fact]
+        public void TestExecuteJob()
+        {
+            using (SqlManagementTestContext context = new SqlManagementTestContext(this))
+            {
+                ResourceGroup resourceGroup = context.CreateResourceGroup();
+                Server server = context.CreateServer(resourceGroup);
+                SqlManagementClient sqlClient = context.GetClient<SqlManagementClient>();
+
+                // Create firewall rule to allow azure service access
+                sqlClient.FirewallRules.CreateOrUpdate(resourceGroup.Name, server.Name, "AllowAzure", new FirewallRule
+                {
+                    StartIpAddress = "0.0.0.0",
+                    EndIpAddress = "0.0.0.0"
+                });
+
+                // Create database
+                string dbName = SqlManagementTestUtilities.GenerateName();
+                var db1 = sqlClient.Databases.CreateOrUpdate(resourceGroup.Name, server.Name, dbName, new Database()
+                {
+                    Location = server.Location,
+                    RequestedServiceObjectiveName = "S1"
+                });
+                Assert.NotNull(db1);
+
+                // Create agent
+                string agentName = "agent1";
+                var agent1 = sqlClient.JobAgents.CreateOrUpdate(resourceGroup.Name, server.Name, agentName, new JobAgent
+                {
+                    Location = server.Location,
+                    DatabaseId = db1.Id
+                });
+
+                // Create job credential
+                string credentialName = "cred1";
+                var credential = sqlClient.JobCredentials.CreateOrUpdate(resourceGroup.Name, server.Name, agentName, credentialName, new JobCredential
+                {
+                    Username = SqlManagementTestUtilities.DefaultLogin,
+                    Password = SqlManagementTestUtilities.DefaultPassword
+                });
+
+                // Create target group
+                string targetGroupName = "targetGroup1";
+                var targetGroup = sqlClient.JobTargetGroups.CreateOrUpdate(resourceGroup.Name, server.Name, agentName, targetGroupName, new JobTargetGroup
+                {
+                    Members = new[]
+                    {
+                        new JobTarget
+                        {
+                            Type = JobTargetType.SqlDatabase,
+                            ServerName = server.Name,
+                            DatabaseName = db1.Name
+                        }
+                    }
+                });
+
+                // Create job
+                string jobName = "job1";
+                var job = sqlClient.Jobs.CreateOrUpdate(resourceGroup.Name, server.Name, agentName, jobName, new Job());
+
+                // Create job step
+                string stepName = "step1";
+                var jobStep = sqlClient.JobSteps.CreateOrUpdate(resourceGroup.Name, server.Name, agentName, jobName, stepName, new JobStep
+                {
+                    TargetGroup = targetGroup.Id,
+                    Credential = credential.Id,
+                    Action = new JobStepAction
+                    {
+                        Value = "select 1"
+                    }
+                });
+
+                // Execute job
+                var execution = sqlClient.JobExecutions.Create(resourceGroup.Name, server.Name, agentName, jobName);
+
+                // Verify execution succeeded
+                Assert.Equal(JobExecutionLifecycle.Succeeded, execution.Lifecycle);
             }
         }
     }
